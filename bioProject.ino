ad2024771dc81a3d5eb86c16c2f0570438d999b9
MaSanto Raffiki <Davidpaez1006@gmail.com>
2018-03-15 20:46:00 -0600

    Create bioProject.ino

---
+++ bioProject.ino
@@ -0,0 +1,21 @@
+body{
+background: repeating-radial-gradient(circle, #f4511e ,#7b1fa2  )
+}
+p{
+	text-align: center; color: white  ; font-size: 36px; font-family: constantia ; margin: 50px;  cursor; crosshair; 
+	font-style: italic;
+}
+
+h1{
+	font-family:constantia; font-size:34px; font-style:italic; color:#000000; text-align:center;
+}
+h2{
+text-align: center; color: black; font-size: 34px; font-family: constantia; text-decoration: overline;
+ border-style: ridge; border-color:  #FFFF33 ; border-style:ridge ;border-style: solid; border-width: 10px;
+}
+h3{
+	
+}
+h4{
+		font-family:constantia; font-size:34px; font-style:italic; color:#000000; text-align:center;
+}
